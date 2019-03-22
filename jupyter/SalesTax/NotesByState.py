fc51bbf6f39748b60bc4a87dac5f0247ae9c044f
Nabeel  Hussain <nabeelh@ayso.org>
2019-03-22 16:53:21 -0700

    updated NotesByState file with extra dummy fields.

--- jupyter/SalesTax/NotesByState.py
+++ jupyter/SalesTax/NotesByState.py
@@ -1,126 +1,176 @@
 NotesByState = {
 						'CA': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None",
 						},
 						'GA': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'HI': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'IL': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'KY': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'MI': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'OK': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'WA': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'WI': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'AL': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'AZ': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'CT': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'FL': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'IA': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'ID': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'KS': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'LA': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'MD': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'NC': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'NE': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'PA': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'SC': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'TN': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'TX': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
 						'WV': {
 								"website": "None",
+								"UN": "None",
+								"PW": "None",
 								"purchaseExemption": "dunno",
 								"notes": "None"
 						},
