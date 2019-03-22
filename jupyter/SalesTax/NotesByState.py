879a07cb6730a9088b172ba15963d8562769d374
Nabeel  Hussain <nabeelh@ayso.org>
2019-03-22 16:30:09 -0700

    Updated Jupyter notebooks. Produced actual Sales Tax files for quarterly states.

---
+++ jupyter/SalesTax/NotesByState.py
@@ -0,0 +1,127 @@
+NotesByState = {
+						'CA': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None",
+						},
+						'GA': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'HI': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'IL': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'KY': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'MI': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'OK': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'WA': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'WI': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'AL': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'AZ': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'CT': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'FL': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'IA': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'ID': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'KS': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'LA': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'MD': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'NC': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'NE': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'PA': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'SC': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'TN': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'TX': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+						'WV': {
+								"website": "None",
+								"purchaseExemption": "dunno",
+								"notes": "None"
+						},
+					}
